3b514d94af65a85c097528926d732ccaf7560a99
RyosukeSomeya <rx.78.0618@gmail.com>
2021-09-19 00:30:23 +0900

    board create 実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,16 @@ class BoardsController < ApplicationController
     # インスタンス変数はviewから参照可能
     @board = Board.new
   end
+
+  def create
+    Board.create(board_params)
+    binding.pry
+  end
+
+  private
+
+  def board_params
+    # 許可したパラメータの内容のみに絞り込み
+    params.require(:board).permit(:name, :title, :body)
+  end
 end
