c911c0c15822aed7246ad3fcb4e4c0290b185afa
RyosukeSomeya <rx.78.0618@gmail.com>
2021-09-23 23:19:59 +0900

    user関連ルーティング、ログイン/ログアウトルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  get 'user/new'
-
-  get 'user/create'
-
-  get 'user/me'
-
-  get 'home/index'
-
-  get 'session/create'
-
-  get 'session/destroy'
+  get 'mypage', to: 'user#me'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy]
 end
